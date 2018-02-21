f0edca18270f7f74b487b3ccb9eeba6e1883660e
Eduardo Luna <lalo.lunagt@gmail.com>
2018-02-21 12:19:18 -0600

    la pendejada de youtube ya quedo

--- p1/public/js/app.js
+++ p1/public/js/app.js
@@ -1,46 +1,4 @@
 function showpage(idpage){
-    if (idpage==0){
-        $("#home").show();
-        $("#customer").hide();
-        $("#shop").hide();
-        $("#map").hide();
-        $("#videos").hide();
-    }
-
-    if (idpage==1){
-        $("#customer").show();
-        $("#home").hide();
-        $("#shop").hide();
-        $("#map").hide();
-        $("#videos").hide();
-    }
-
-    if (idpage==2){
-        $("#shop").show();
-        $("#customer").hide();
-        $("#home").hide();
-        $("#map").hide();
-        $("#videos").hide();
-    }
-
-    if (idpage==3){
-        $("#map").show();
-        $("#videos").hide();
-        $("#shop").hide();
-        $("#customer").hide();
-        $("#home").hide();
-    }
-
-    if (idpage==4){
-        $("#map").hide();
-        $("#videos").show();
-        $("#shop").hide();
-        $("#customer").hide();
-        $("#home").hide();
-    }
-}
-
-function showpage2(idpage){
     $("#home").hide();
     $("#customer").hide();
     $("#shop").hide();
