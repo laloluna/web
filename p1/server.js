374648411bb3d87b1d42a6ef333a10b63f5c9849
Eduardo Luna <lalo.lunagt@gmail.com>
2018-02-21 09:15:43 -0600

    Cambio de Puerto 2.0

--- p1/server.js
+++ p1/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.set("port", (process.env.PORT || 8088));
+app.set("port", (process.env.PORT || 8080));
 
 app.use("/public", express.static(path.join(__dirname + "/public")));
 
