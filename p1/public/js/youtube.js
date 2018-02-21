2d56bda4cfdc09f8bbdbfa168b75c94ccffd2832
Eduardo Luna <lalo.lunagt@gmail.com>
2018-02-21 11:12:32 -0600

    Youtube 1

---
+++ p1/public/js/youtube.js
@@ -0,0 +1,16 @@
+function getVideos() {
+    $.getJSON('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&type=video&channelId=UCAW-NpUFkMyCNrvRSSGIvDQ=20&key=AIzaSyAfK23yKsbz8VdteM5R2GE6kf98dThidTY', function(data) {
+        var items = data.items;
+        var ids = [];
+        items.forEach(element => {
+            ids.push(element.id.videoId);
+        });
+        placeVideos(ids);
+    })
+}
+
+function placeVideos(ids) {
+    ids.forEach(element => {
+        $( "#youtube" ).append( '<iframe class="col-sm-3" width="420" height="315"src="https://www.youtube.com/embed/' + element + '"></iframe>' );
+    });
+}
